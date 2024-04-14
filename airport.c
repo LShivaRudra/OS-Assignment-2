eb0daeb7ba80d6e17fb5c6a49b58c48de5406404
LShivaRudra <shivarudralolla@gmail.com>
2024-04-14 22:13:30 +0530

    updated airport.c,to check termination and mutex once more

--- airport.c
+++ airport.c
@@ -16,7 +16,11 @@
 #define MIN_DIFF 16000
 
 pthread_t tids[MAX_PLANES];//max number of planes is 10
-pthread_mutex_t lock;
+pthread_mutex_t runway_mutex[MAX_RUNWAYS];
+
+//msg queue
+key_t key;
+int msgid;
 
 struct msg_data{
     int airport_num_arrival;
@@ -68,8 +72,50 @@ void* thread_func(void *arg){
     }
 
     printf("Selected Runway is: %d\n",selected_runway);
+    
+    pthread_mutex_lock(&runway_mutex[selected_runway]);
 
+    if(msg->recv_data.data.airport_num_departure==msg->airport_num){
+        //Simulate boarding/loading
+        sleep(3);
+
+        //Simulate takeoff
+        sleep(2);
+        
+        //Inform the ATC
+        struct msgbuf msg_send_atc;
+        msg_send_atc.data=msg->recv_data.data;
+        msg_send_atc.data.departure_status=1;
+        msg_send_atc.data.arrival_status=0;
+        msg_send_atc.msg_type=msg->airport_num+10;
+        msgsnd(msgid,&msg_send_atc,sizeof(msg_send_atc),0);
+
+        printf("Plane %d has completed boarding/loading and taken off from Runway No. %d of Airport No. %d.\n",msg->recv_data.data.plane_id,selected_runway,msg->airport_num);
+    }
+    else if(msg->recv_data.data.airport_num_arrival==msg->airport_num){
+        //Simulate plane travel
+        sleep(30);
+
+        //Simulate landing
+        sleep(2);
+
+        //Simulate deboarding/unloading
+        sleep(3);
+
+        //Inform ATC
+        struct msgbuf msg_send_atc;
+        msg_send_atc.data=msg->recv_data.data;
+        msg_send_atc.data.departure_status=1;
+        msg_send_atc.data.arrival_status=1;
+        msg_send_atc.msg_type=msg->airport_num+10;
+        msgsnd(msgid,&msg_send_atc,sizeof(msg_send_atc),0);
+
+        printf("Plane %d has landed on Runway No. %d of Airport No. %d and has completed deboarding/unloading.\n",msg->recv_data.data.plane_id,selected_runway,msg->airport_num);
+    }
+
+    pthread_mutex_unlock(&runway_mutex[selected_runway]);
     fflush(stdout);
+    pthread_exit(NULL);
 }
 
 int main(){
@@ -92,9 +138,6 @@ int main(){
     }
     airport.runway_capacities[num_of_runways]=15000;//backup runway
 
-    //msg queue
-    key_t key;
-    int msgid;
     // ftok to generate unique key 
     key = ftok("airtrafficcontroller.c", 'A');
     msgid = msgget(key, 0666 | IPC_CREAT); 
